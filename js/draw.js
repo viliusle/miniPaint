2973c303f377cab6698e21655bf1f128aa393fbb
viliusle <vilius@192.168.100.113>
2014-04-23 13:55:06 +0300

    resize and color picker update

--- js/draw.js
+++ js/draw.js
@@ -784,53 +784,59 @@ function DRAW_CLASS(){
 		};
 	//hermite resample - classic "rings.gif" 1000x1000 resize to 200x200 record -  0.040
 	this.resample_hermite = function(canvas, W, H, W2, H2){
-		var time1 = Date.now();
-		var img = canvas.getContext("2d").getImageData(0, 0, W, H);
-		var img2 = canvas.getContext("2d").getImageData(0, 0, W2, H2);
-		var data = img.data;
-		var data2 = img2.data;
-		var ratio_w = W / W2;
-		var ratio_h = H / H2;
-		var ratio_w_half = Math.ceil(ratio_w/2);
-		var ratio_h_half = Math.ceil(ratio_h/2);
-		
-		for(var j = 0; j < H2; j++){
-			for(var i = 0; i < W2; i++){
-				var x2 = (i + j*W2) * 4;
-				var weight = 0;
-				var weights = 0;
-				var gx_r = gx_g = gx_b = gx_a = 0;
-				var center_y = (j + 0.5) * ratio_h;
-				for(var yy = Math.floor(j * ratio_h); yy < (j + 1) * ratio_h; yy++){
-					var dy = Math.abs(center_y - (yy + 0.5)) / ratio_h_half;
-					var center_x = (i + 0.5) * ratio_w;
-					var w0 = dy*dy //pre-calc part of w
-					for(var xx = Math.floor(i * ratio_w); xx < (i + 1) * ratio_w; xx++){
-						var dx = Math.abs(center_x - (xx + 0.5)) / ratio_w_half;
-						var w = Math.sqrt(w0 + dx*dx);
-						if(w >= -1 && w <= 1){
-							//hermite filter
-							weight = 2 * w*w*w - 3*w*w + 1;
-							if(weight > 0){
-								dx = 4*(xx + yy*W);
-								gx_r += weight * data[dx];
-								gx_g += weight * data[dx + 1];
-								gx_b += weight * data[dx + 2];
-								gx_a += weight * data[dx + 3];
-								weights += weight;
-								}
+	var time1 = Date.now();
+	var img = canvas.getContext("2d").getImageData(0, 0, W, H);
+	var img2 = canvas.getContext("2d").getImageData(0, 0, W2, H2);
+	var data = img.data;
+	var data2 = img2.data;
+	var ratio_w = W / W2;
+	var ratio_h = H / H2;
+	var ratio_w_half = Math.ceil(ratio_w/2);
+	var ratio_h_half = Math.ceil(ratio_h/2);
+	
+	for(var j = 0; j < H2; j++){
+		for(var i = 0; i < W2; i++){
+			var x2 = (i + j*W2) * 4;
+			var weight = 0;
+			var weights = 0;
+			var weights_alpha = 0;
+			var gx_r = gx_g = gx_b = gx_a = 0;
+			var center_y = (j + 0.5) * ratio_h;
+			for(var yy = Math.floor(j * ratio_h); yy < (j + 1) * ratio_h; yy++){
+				var dy = Math.abs(center_y - (yy + 0.5)) / ratio_h_half;
+				var center_x = (i + 0.5) * ratio_w;
+				var w0 = dy*dy //pre-calc part of w
+				for(var xx = Math.floor(i * ratio_w); xx < (i + 1) * ratio_w; xx++){
+					var dx = Math.abs(center_x - (xx + 0.5)) / ratio_w_half;
+					var w = Math.sqrt(w0 + dx*dx);
+					if(w >= -1 && w <= 1){
+						//hermite filter
+						weight = 2 * w*w*w - 3*w*w + 1;
+						if(weight > 0){
+							dx = 4*(xx + yy*W);
+							//alpha
+							gx_a += weight * data[dx + 3];
+							weights_alpha += weight;
+							//colors
+							if(data[dx + 3] < 255)
+								weight = weight * data[dx + 3] / 250;
+							gx_r += weight * data[dx];
+							gx_g += weight * data[dx + 1];
+							gx_b += weight * data[dx + 2];
+							weights += weight;
 							}
-						}		
-					}
-				data2[x2]     = gx_r / weights;
-				data2[x2 + 1] = gx_g / weights;
-				data2[x2 + 2] = gx_b / weights;
-				data2[x2 + 3] = gx_a / weights;
+						}
+					}		
 				}
+			data2[x2]     = gx_r / weights;
+			data2[x2 + 1] = gx_g / weights;
+			data2[x2 + 2] = gx_b / weights;
+			data2[x2 + 3] = gx_a / weights_alpha;
 			}
-		console.log("hermite = "+(Math.round(Date.now() - time1)/1000)+" s");
-		canvas.getContext("2d").clearRect(0, 0, Math.max(W, W2), Math.max(H, H2));
-		canvas.getContext("2d").putImageData(img2, 0, 0);
+		}
+	console.log("hermite = "+(Math.round(Date.now() - time1)/1000)+" s");
+	canvas.getContext("2d").clearRect(0, 0, Math.max(W, W2), Math.max(H, H2));
+	canvas.getContext("2d").putImageData(img2, 0, 0);
 		};
 	this.resample_hermite_threads = function(canvas, W, H, W2, H2){
 		var time1 = Date.now();
