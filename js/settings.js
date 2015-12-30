ca84a993adf50f2911a829f16bd139c70c4821c3
viliuslevickas <vilius@enigmasoftware.com>
2015-12-30 12:39:30 +0200

    version update

--- js/settings.js
+++ js/settings.js
@@ -9,7 +9,7 @@ var canvas_preview = document.getElementById("canvas_preview").getContext("2d");
 //settings
 var AUTHOR = 'ViliusL';
 var EMAIL = 'www.viliusl@gmail.com';
-var VERSION = '2.2';
+var VERSION = '2.2.1';
 var WIDTH = 800;				//canvas midth
 var HEIGHT = 600;				//canvas height
 var RATIO = WIDTH/HEIGHT;			//width & height ratio
