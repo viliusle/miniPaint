5ee74e261f49436a13d29384d2ed4b621fdf3e7e
viliusle <www.viliusl@gmail.com>
2021-03-07 23:47:18 +0200

    fixed color #ffffffff handling

--- src/js/libs/helpers.js
+++ src/js/libs/helpers.js
@@ -178,9 +178,9 @@ class Helper_class {
 		}
 		var triplets = /^([a-f0-9]{2})([a-f0-9]{2})([a-f0-9]{2})$/i.exec(hex).slice(1);
 		return {
-			r: parseInt(triplets[0], 16),
-			g: parseInt(triplets[1], 16),
-			b: parseInt(triplets[2], 16),
+			r: parseInt(triplets[1], 16),
+			g: parseInt(triplets[2], 16),
+			b: parseInt(triplets[3], 16),
 			a: 255
 		};
 	}
