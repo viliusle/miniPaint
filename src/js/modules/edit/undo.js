8884e13a9b3678cc5c1010ee86c8acacaf6fb785
viliusle <www.viliusl@gmail.com>
2021-06-20 00:31:03 +0300

    undo button for mobile

--- src/js/modules/edit/undo.js
+++ src/js/modules/edit/undo.js
@@ -1,9 +1,26 @@
 import Base_state_class from './../../core/base-state.js';
 
+var instance = null;
+
 class Edit_undo_class {
 
 	constructor() {
+		//singleton
+		if (instance) {
+			return instance;
+		}
+		instance = this;
+
 		this.Base_state = new Base_state_class();
+		this.events();
+	}
+
+	events(){
+		var _this = this;
+
+		document.querySelector('#undo_button').addEventListener('click', function (event) {
+			_this.Base_state.undo();
+		});
 	}
 
 	undo() {
